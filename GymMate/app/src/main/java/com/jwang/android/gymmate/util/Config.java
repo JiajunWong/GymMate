add9b5f0a91c4d70dbb28f88ef838e4f8cbbd517
JiajunWong <sopherwang@gmail.com>
2015-06-25 23:01:59 -0700

    add Gym app to manifest file

---
+++ GymMate/app/src/main/java/com/jwang/android/gymmate/util/Config.java
@@ -0,0 +1,10 @@
+package com.jwang.android.gymmate.util;
+
+/**
+ * Created by jiajunwang on 6/25/15.
+ */
+public class Config
+{
+    public static final String TEST_GYM_LONG = "37.549696";
+    public static final String TEST_GYM_LAT = "-122.314780";
+}

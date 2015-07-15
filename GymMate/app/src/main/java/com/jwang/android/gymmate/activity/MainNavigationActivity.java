939f603e80864c2ecba50dc3b660d720ae62c437
Jiajun Wang <jwang@stumbleupon.com>
2015-07-15 11:23:56 -0700

    add navigation icon

--- GymMate/app/src/main/java/com/jwang/android/gymmate/activity/MainNavigationActivity.java
+++ GymMate/app/src/main/java/com/jwang/android/gymmate/activity/MainNavigationActivity.java
@@ -1,6 +1,9 @@
 package com.jwang.android.gymmate.activity;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.jwang.android.gymmate.R;
 import com.jwang.android.gymmate.adapter.MediaSyncAdapter;
@@ -12,11 +15,18 @@ import com.jwang.android.gymmate.fragment.MainMediaListFragment;
  */
 public class MainNavigationActivity extends BaseActivity
 {
+    protected DrawerLayout mDrawerLayout;
+    protected View mDrawerRootLeft;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_navigation);
+        mToolbar.setNavigationIcon(R.drawable.ic_action_navigation_menu);
+        mDrawerRootLeft = findViewById(R.id.drawer_layout_left);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
         MediaSyncAdapter.initializeSyncAdapter(this);
     }
 
@@ -27,4 +37,51 @@ public class MainNavigationActivity extends BaseActivity
         MainMediaListFragment mediaListFragment = (MainMediaListFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_media_list);
         mediaListFragment.refreshData();
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                toggleLeftDrawer();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void toggleLeftDrawer()
+    {
+        if (mDrawerLayout.isDrawerOpen(mDrawerRootLeft))
+        {
+            mDrawerLayout.closeDrawer(mDrawerRootLeft);
+        }
+        else
+        {
+            mDrawerLayout.openDrawer(mDrawerRootLeft);
+        }
+    }
+
+    public boolean isLeftDrawerOpen()
+    {
+        if (mDrawerLayout == null)
+        {
+            return false;
+        }
+        return (mDrawerLayout.isDrawerOpen(mDrawerRootLeft));
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        if (isLeftDrawerOpen())
+        {
+            toggleLeftDrawer();
+        }
+        else
+        {
+            super.onBackPressed();
+        }
+    }
+
 }
